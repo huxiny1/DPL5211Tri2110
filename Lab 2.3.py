80235bcbd337d54882364340c65a1a995fbebace
huxiny1 <90165802+huxiny1@users.noreply.github.com>
2021-09-13 10:46:46 +0800

    Add files via upload

---
+++ Lab 2.3.py
@@ -0,0 +1,7 @@
+#Student ID: 1201201432
+#Student Name: Joyce Hu Xin Yi
+
+celsius = float(input("Enter Temperature: "))
+
+fahrenheit = (celsius * 1.8) + 32
+print("{:.1f} degree Celsius is equal to {:.1f} degree Fahrenheit".format(celsius,fahrenheit))
